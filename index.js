181cb4f8fde0a6fd8f69e8dab155fc159496ac5a
Ian Kabaka <iankabaka1@gmail.com>
2024-09-24 13:11:29 +0300

    arithmetic operations

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 7) + 1;
+
+let num3 = 9;
+let num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(5, 9, 15, 16, 20);
